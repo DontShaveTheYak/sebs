696ab527fb4ce00f154a9e456c55f0303cb6d4b8
Levi Blaney <shadycuz@gmail.com>
2020-04-12 00:48:19 +0530

    add rough app outline

--- sebs/main.py
+++ sebs/main.py
@@ -7,7 +7,9 @@ __authors__ = ["Levi Blaney", "Neha Singh"]
 __version__ = "0.1.0"
 __license__ = "GPLv3"
 
+import sys
 import argparse
+from ec2 import Instance
 
 
 def main(args):
@@ -15,15 +17,34 @@ def main(args):
     print("hello world")
     print(args)
 
+    # Get a handler for the current EC2 instance
+    server = Instance()
+
+    # Add the requested Stateful Devices to the server
+    for device in args.backup:
+        server.add_stateful_device(device)
+
+    # Make sure the Stateful Volumes are attached to this server
+    server.attach_stateful_volumes()
+
+    # Tag the Stateful Volumes so they can be found on next boot
+    server.tag_stateful_volumes()
+
+    # I think we done?
+
+    sys.exit()
+
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-b','--backup', action='append', help='<Required> List of Devices to Backup', required=True)
+    parser.add_argument('-b', '--backup', action='append',
+                        help='<Required> List of Devices to Backup', required=True)
 
     # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-n", "--name", action="store", dest="name")
+    parser.add_argument("-n", "--name", default='sebs',
+                        help='<Optional> specify a unique name.')
 
     # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
     parser.add_argument(
@@ -41,4 +62,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
-
